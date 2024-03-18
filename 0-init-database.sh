609f566778014ce24c36dd98b48c41ec403ca205
Cascadeflorent <florent@cascade.lu>
2024-03-18 10:49:28 +0100

    try to chmod directly inside the script

--- 0-init-database.sh
+++ 0-init-database.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CASCADE_DB_PASSWORD=$(cat /run/secrets/db_password)
+chmod 600 /run/secrets/db_password
 
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
     CREATE ROLE cascade WITH LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE
-    NOINHERIT REPLICATION CONNECTION LIMIT -1 PASSWORD '$CASCADE_DB_PASSWORD';
+    NOINHERIT REPLICATION CONNECTION LIMIT -1 PASSWORD '$(< /run/secrets/db_password)';
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
